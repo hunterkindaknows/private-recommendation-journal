21b8eea89b31fae34cdbbaa28902613e2d0b586a
hunterkindaknows <hunterkindaknows@gmail.com>
2026-02-18 21:37:56 -0800

    Add sitewide WebAudio lounge generator with saved controls

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Cormorant_Garamond, Inter, Dancing_Script } from "next/font/google"
 import "./globals.css"
 import { SiteHeader } from "@/components/site-header"
 import { SiteFooter } from "@/components/site-footer"
+import { LoungeAudio } from "@/components/lounge-audio"
 
 const serif = Cormorant_Garamond({
   subsets: ["latin"],
@@ -78,6 +79,7 @@ export default function RootLayout({
         <SiteHeader />
         <main>{children}</main>
         <SiteFooter />
+        <LoungeAudio />
       </body>
     </html>
   )
